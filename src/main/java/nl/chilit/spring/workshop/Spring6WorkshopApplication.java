70a4c578d8f282128f12315c199cdf0db7496322
Tubby <git@tubby.nl>
2023-04-13 21:23:31 +0200

    custom ProblemDetail voor TooManyItemsException

--- src/main/java/nl/chilit/spring/workshop/Spring6WorkshopApplication.java
+++ src/main/java/nl/chilit/spring/workshop/Spring6WorkshopApplication.java
@@ -4,6 +4,12 @@ import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @SpringBootApplication
 public class Spring6WorkshopApplication {
@@ -15,5 +21,14 @@ public class Spring6WorkshopApplication {
     @Bean
     public ModelMapper modelMapper() {
         return new ModelMapper();
+    }
+
+    @ControllerAdvice
+    static class FancyProblemDetailsAdvice extends ResponseEntityExceptionHandler {
+        @ExceptionHandler(TooManyItemsException.class)
+        public ProblemDetail handleCustomException(TooManyItemsException tmi) {
+            return ProblemDetail
+                    .forStatusAndDetail(HttpStatus.BAD_REQUEST, tmi.getMaxItems()+" is the max jongeh");
+        }
     }
 }
